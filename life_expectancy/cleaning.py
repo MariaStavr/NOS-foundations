e6878a4e8d7aed7c13ff20c2bf871cc015e5badf
MariaStavr <mastavrianaki@gmail.com>
2022-10-08 02:41:36 +0100

    folder structure change

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -1,8 +1,8 @@
 """
 Provides data cleansing
 """
-import argparse
-import sys
+# import argparse
+# import sys
 import os
 import pandas as pd
 
@@ -12,6 +12,7 @@ SAVE_FILE_NAME = 'pt_life_expectancy.csv'
 
 def clean_data(path, imported_file_name, saved_file_name): # pylint: disable=C0116
 
+    print(dir_path)
     _df = pd.read_csv(f'{path}\\data\\{imported_file_name}', sep='\t', engine='python')
     _df[['unit','sex','age','region']] = _df.iloc[:, 0].str.split(',', expand=True)
     _df = _df.drop(_df.columns[0], axis=1)
@@ -26,11 +27,11 @@ def clean_data(path, imported_file_name, saved_file_name): # pylint: disable=C01
     data.to_csv(f'{path}\\data\\{saved_file_name}', index=False)
 
 
-def parse_args(args):  # pylint: disable=C0116
-    parser_aux = argparse.ArgumentParser()
-    parser_aux.add_argument('--country', type=str, default="PT")
-    return parser.parse_args(args)
+# def parse_args(args):  # pylint: disable=C0116
+#     parser_aux = argparse.ArgumentParser()
+#     parser_aux.add_argument('--country', type=str, default="PT")
+#     return parser.parse_args(args)
 
-if __name__ == "__main__": # pragma: no cover
-    parser = parse_args(sys.argv[1:])
-    clean_data(dir_path, IMPORT_FILE_NAME, SAVE_FILE_NAME)
+#if __name__ == "__main__": # pragma: no cover
+    #parser = parse_args(sys.argv[1:])
+clean_data(dir_path, IMPORT_FILE_NAME, SAVE_FILE_NAME)

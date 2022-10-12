910b554c8d94e87c3269de48c8d53325a2a5e16b
MariaStavr <mastavrianaki@gmail.com>
2022-10-12 04:09:48 +0100

    Test reverted to original, cleaning.py updates

--- life_expectancy/tests/test_cleaning.py
+++ life_expectancy/tests/test_cleaning.py
@@ -1,24 +1,16 @@
 """Tests for the cleaning module"""
-from pathlib import Path
 import pandas as pd
+
 from life_expectancy.cleaning import clean_data
 from . import OUTPUT_DIR
-#, parse_args
 
-def test_clean_data(pt_life_expectancy_expected): # pylint: disable=C0116
+
+def test_clean_data(pt_life_expectancy_expected):
     """Run the `clean_data` function and compare the output to the expected output"""
-    #path='C:\\Users\\stavr\\OneDrive\\Desktop\\NOS Foundations\\assignments\\life_expectancy'
-    path=Path(__file__).parent.parent
-    imported_file='eu_life_expectancy_raw.tsv'
-    saved_file='saved'
-    clean_data(path,imported_file, saved_file )
+    clean_data()
     pt_life_expectancy_actual = pd.read_csv(
         OUTPUT_DIR / "pt_life_expectancy.csv"
     )
     pd.testing.assert_frame_equal(
         pt_life_expectancy_actual, pt_life_expectancy_expected
     )
-
-# def test_parser(self): # pylint: disable=C0116
-#     parser = parse_args(['--country', 'PT'])
-#     self.assertTrue(parser.long)

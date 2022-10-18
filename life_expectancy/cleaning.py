3713e7867704ff02aabe50487ea8f0e9f3de8ce8
MariaStavr <mastavrianaki@gmail.com>
2022-10-18 04:04:12 +0100

    refactoring completed

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -2,6 +2,7 @@
 Provides data cleansing
 """
 import argparse
+import csv
 from pathlib import Path
 
 import pandas as pd
@@ -11,9 +12,12 @@ IMPORT_FILE_NAME = "eu_life_expectancy_raw.tsv"
 SAVE_FILE_NAME = "pt_life_expectancy.csv"
 
 
-def clean_data(region_name: str):  # pylint: disable=C0116
-
+def load_data() -> pd.DataFrame:  # pylint: disable=C0116
     _df = pd.read_csv(DIR_PATH / "data" / IMPORT_FILE_NAME, delimiter="\t")
+    return _df
+
+
+def clean_data(_df: pd.DataFrame, region_name: str) -> pd.DataFrame:  # pylint: disable=C0116
     _df[['unit', 'sex', 'age', 'region']
         ] = _df.iloc[:, 0].str.split(',', expand=True)
     _df = _df.drop(_df.columns[0], axis=1)
@@ -26,16 +30,32 @@ def clean_data(region_name: str):  # pylint: disable=C0116
     data = data[data['value'] != ' ']
     data = data[data['region'] == region_name]
     data['value'] = data['value'].astype(float)
+    return data
+
+
+def save_data(data) -> csv:  # pylint: disable=C0116
     data.to_csv(DIR_PATH / "data" / SAVE_FILE_NAME, index=False)
 
 
 def parse_args():  # pylint: disable=C0116
+    '''
+    Adding command line option for region.
+    '''
     parser_aux = argparse.ArgumentParser()
     parser_aux.add_argument('--region', type=str, default="PT")
     args = parser_aux.parse_args()
     return args.region
 
 
+def main(region_name):
+    '''
+    Loads, cleans and saves the data.
+    '''
+    _df = load_data()
+    cleaned_df = clean_data(_df, region_name)
+    save_data(cleaned_df)
+
+
 if __name__ == "__main__":  # pragma: no cover
     region = parse_args()
-    clean_data(region)
+    main(region)
